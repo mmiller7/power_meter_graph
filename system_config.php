8ac5fe331f814d366302d4bca49ade11f2a0bac3
Matthew Miller <mmiller7@mail.umw.edu>
2018-02-18 17:20:00 -0500

    Added missing PHP tags

--- system_config.php
+++ system_config.php
@@ -1,7 +1,8 @@
-
+<?php
 //System dependant configuration for reading/writing to the database
 
 $hourly_db_handle  = new SQLite3('meter_readings.sqlite3.db');
 $minute_db_handle  = new SQLite3('minute_meter_readings.sqlite3.db');
 
 date_default_timezone_set('America/New_York');
+?>
